332e4a1adc95817f249ea4181923de8af2e5c075
Jaime Gago <gagojaime@gmail.com>
2026-02-16 11:40:27 +0100

    fix: lint

--- internal/graph/relations.go
+++ internal/graph/relations.go
@@ -2,11 +2,11 @@ package graph
 
 // Relation constants for graph edges.
 const (
-	RelationMetricsIn  = "metrics_in"
-	RelationLogsIn     = "logs_in"
-	RelationTracesIn   = "traces_in"
-	RelationAlertsIn   = "alerts_in"
-	RelationPagedVia   = "paged_via"
-	RelationDashboard = "dashboard_in"
-	RelationIsK8sNode  = "is_k8s_node"
+	RelationMetricsIn   = "metrics_in"
+	RelationLogsIn      = "logs_in"
+	RelationTracesIn    = "traces_in"
+	RelationAlertsIn    = "alerts_in"
+	RelationPagedVia    = "paged_via"
+	RelationDashboardIn = "dashboard_in"
+	RelationIsK8sNode   = "is_k8s_node"
 )
